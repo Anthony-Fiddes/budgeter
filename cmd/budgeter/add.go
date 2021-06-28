e06db1927141c565c92ab56f22e56b069507a381
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2021-06-28 07:31:53 -0400

    Simplify interactive add function

--- cmd/budgeter/add.go
+++ cmd/budgeter/add.go
@@ -8,11 +8,13 @@ import (
 	"github.com/Anthony-Fiddes/budgeter/internal/models"
 )
 
-const addName = "add"
+const (
+	addName = "add"
+)
 
 // TODO: Find a way to handle duplicates gracefully
-func interactiveAdd() (models.Transaction, error) {
-	addField := func(field string) (string, error) {
+func interactiveAdd(db *models.DB) error {
+	getField := func(field string) (string, error) {
 		fmt.Printf("%s: ", field)
 		response, err := inpt.Line()
 		if err != nil {
@@ -25,64 +27,72 @@ func interactiveAdd() (models.Transaction, error) {
 	// TODO: perhaps implement a date picker? / start the next date where you
 	// left off with the last one / allow short dates like "21" or "6/21" that
 	// default to this month or year
-	tx := models.Transaction{}
-	date, err := addField(models.TransactionDateCol)
-	if err != nil {
-		return models.Transaction{}, err
+	getTransaction := func() (models.Transaction, error) {
+		tx := models.Transaction{}
+		date, err := getField(models.TransactionDateCol)
+		if err != nil {
+			return models.Transaction{}, err
+		}
+		tx.Date, err = models.Date(date)
+		if err != nil {
+			return models.Transaction{}, err
+		}
+		tx.Entity, err = getField(models.TransactionEntityCol)
+		if err != nil {
+			return models.Transaction{}, err
+		}
+		amount, err := getField(models.TransactionAmountCol)
+		if err != nil {
+			return models.Transaction{}, err
+		}
+		tx.Amount, err = models.Cents(amount)
+		if err != nil {
+			return models.Transaction{}, err
+		}
+		tx.Note, err = getField(models.TransactionNoteCol)
+		if err != nil {
+			return models.Transaction{}, err
+		}
+		return tx, nil
 	}
-	tx.Date, err = models.Date(date)
-	if err != nil {
-		return models.Transaction{}, err
+
+	for {
+		tx, err := getTransaction()
+		if err != nil {
+			return err
+		}
+		if _, err := db.InsertTransaction(tx); err != nil {
+			return err
+		}
+
+		// TODO: Add context when adding transactions. e.g. making the last
+		// used date the new default?, enabling an undo command
+		// TODO: When adding a transaction, maybe show a couple of
+		// transactions from around the same time?
+		fmt.Print("\nWould you like to add another transaction? (y/[n]) ")
+		confirmed, err := inpt.Confirm()
+		fmt.Println()
+		if err != nil {
+			return err
+		}
+		if !confirmed {
+			break
+		}
 	}
-	tx.Entity, err = addField(models.TransactionEntityCol)
-	if err != nil {
-		return models.Transaction{}, err
-	}
-	amount, err := addField(models.TransactionAmountCol)
-	if err != nil {
-		return models.Transaction{}, err
-	}
-	tx.Amount, err = models.Cents(amount)
-	if err != nil {
-		return models.Transaction{}, err
-	}
-	tx.Note, err = addField(models.TransactionNoteCol)
-	if err != nil {
-		return models.Transaction{}, err
-	}
-	return tx, nil
+
+	return nil
 }
 
 func add(db *models.DB, cmdArgs []string) error {
-	var err error
 	fs := flag.NewFlagSet(ingestName, flag.ContinueOnError)
-	err = fs.Parse(cmdArgs)
-	if err != nil {
+	if err := fs.Parse(cmdArgs); err != nil {
 		return err
 	}
 	args := fs.Args()
 	if len(args) == 0 {
-		for {
-			tx, err := interactiveAdd()
-			if err != nil {
-				return err
-			}
-			if _, err := db.InsertTransaction(tx); err != nil {
-				return err
-			}
-
-			// TODO: Add context when adding transactions. e.g. making the last
-			// used date the new default?, enabling an undo command
-			fmt.Print("\nWould you like to add another transaction? ")
-			confirmed, err := inpt.Confirm()
-			fmt.Println()
-			if err != nil {
-				return err
-			}
-			if !confirmed {
-				break
-			}
-		}
+		return interactiveAdd(db)
+	} else if len(args) > fieldsPerRecord {
+		return fmt.Errorf("%s takes at most %d arguments", addName, fieldsPerRecord)
 	}
 	// TODO: implement an option that parses from flags or from args
 	return nil
