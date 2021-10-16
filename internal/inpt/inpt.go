ff6ee5be69b8466f71d0c3d43e94da9753c4c801
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2021-10-15 22:24:37 -0400

    Refactor inpt to wrap bufio.Scanner in a struct

--- internal/inpt/inpt.go
+++ internal/inpt/inpt.go
@@ -3,14 +3,15 @@ package inpt
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
-var scanner *bufio.Scanner
+var scanner *Scanner
 
 func init() {
-	scanner = bufio.NewScanner(os.Stdin)
+	scanner = NewScanner(os.Stdin)
 }
 
 // Normalize trims the leading and trailing whitespace of a string and makes all
@@ -21,9 +22,20 @@ func Normalize(str string) string {
 	return str
 }
 
-// Line reads a line from stdin and trims the whitespace around it.
-func Line() (string, error) {
-	scanner.Scan()
+// Scanner is a wrapper around bufio.Scanner with convenient methods for
+// collecting user input and cleaning it up.
+type Scanner struct {
+	*bufio.Scanner
+}
+
+// NewScanner returns a Scanner of the supplied reader.
+func NewScanner(r io.Reader) *Scanner {
+	return &Scanner{bufio.NewScanner(r)}
+}
+
+// Line reads a line from the scanner and trims the whitespace around it.
+func (s *Scanner) Line() (string, error) {
+	s.Scan()
 	if err := scanner.Err(); err != nil {
 		return "", fmt.Errorf("could not read line: %w", err)
 	}
@@ -32,10 +44,10 @@ func Line() (string, error) {
 	return result, nil
 }
 
-// Confirm reads input from the user and returns true if it is "y" or false if
+// Confirm reads input from the scanner and returns true if it is "y" or false if
 // it is anything else.
-func Confirm() (bool, error) {
-	input, err := Line()
+func (s *Scanner) Confirm() (bool, error) {
+	input, err := s.Line()
 	if err != nil {
 		return false, fmt.Errorf("could not get user confirmation: %w", err)
 	}
@@ -44,4 +56,15 @@ func Confirm() (bool, error) {
 		return false, nil
 	}
 	return true, nil
+}
+
+// Line reads a line from stdin and trims the whitespace around it.
+func Line() (string, error) {
+	return scanner.Line()
+}
+
+// Confirm reads input from the user and returns true if it is "y" or false if
+// it is anything else.
+func Confirm() (bool, error) {
+	return scanner.Confirm()
 }
