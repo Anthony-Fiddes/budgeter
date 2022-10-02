0b40e1166cb3db32bc187144a7648cf70ed87478
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2022-10-02 13:00:58 -0700

    Update convert to work with the new command system on main

--- cli/budgeter/convert.go
+++ cli/budgeter/convert.go
@@ -1,6 +1,7 @@
 package budgeter
 
 import (
+	"fmt"
 	"os"
 	"path"
 
@@ -8,27 +9,25 @@ import (
 	"github.com/Anthony-Fiddes/budgeter/model/transaction"
 )
 
-var convertUsage = "convert takes a CSV from a financial provider and simplifies it to the budgeter format"
-
 type convert struct{}
 
-func (c *convert) Name() string {
+func (c convert) Name() string {
 	return "convert"
 }
 
-func (c *convert) Run(cli *CLI) int {
+func (c convert) Usage() string {
+	return "convert takes a CSV from a financial provider and simplifies it to the budgeter format"
+}
+
+func (c convert) Run(cmdArgs []string) error {
 	fs := getFlagset(c.Name())
-	if err := fs.Parse(cli.args); err != nil {
-		cli.logParsingErr(err)
-		cli.err.Println()
-		cli.err.Print(convertUsage)
-		return 1
+	if err := fs.Parse(cmdArgs); err != nil {
+		return err
 	}
 	// TODO: guess the file format from its name or other details
 	args := fs.Args()
 	if len(args) < 2 {
-		cli.err.Printf("%s takes at least two arguments (the name of the file to be converted and its format)", c.Name())
-		return 1
+		return fmt.Errorf("%s takes at least two arguments (the name of the file to be converted and its format)", c.Name())
 	}
 
 	inputPath := args[0]
@@ -36,8 +35,7 @@ func (c *convert) Run(cli *CLI) int {
 	f, err := os.Open(args[0])
 	defer f.Close()
 	if err != nil {
-		cli.err.Println(err)
-		return 1
+		return err
 	}
 
 	var transactions []transaction.Transaction
@@ -46,23 +44,23 @@ func (c *convert) Run(cli *CLI) int {
 		chase := csvfmt.NewChaseReader(f)
 		transactions, err = csvfmt.ReadAll(chase)
 		if err != nil {
-			cli.err.Printf(`could not read transactions from %s formatted file %s: %v`, csvfmt.Chase, inputPath, err)
+			return fmt.Errorf(`could not read transactions from %s formatted file %s: %w`, csvfmt.Chase, inputPath, err)
 		}
 	case csvfmt.CapitalOne:
 		capitalone := csvfmt.NewCapitalOneReader(f)
 		transactions, err = csvfmt.ReadAll(capitalone)
 		if err != nil {
-			cli.err.Printf(`could not read transactions from %s formatted file %s: %v`, csvfmt.CapitalOne, inputPath, err)
+			return fmt.Errorf(`could not read transactions from %s formatted file %s: %w`, csvfmt.CapitalOne, inputPath, err)
 		}
 	case csvfmt.Venmo:
 		venmo := csvfmt.NewVenmoReader(f)
 		transactions, err = csvfmt.ReadAll(venmo)
 		if err != nil {
-			cli.err.Printf(`could not read transactions from %s formatted file %s: %v`, csvfmt.Venmo, inputPath, err)
+			return fmt.Errorf(`could not read transactions from %s formatted file %s: %w`, csvfmt.Venmo, inputPath, err)
 		}
 	default:
-		// TODO: List supported formats
-		cli.err.Printf(`format "%s" is not supported`, format)
+		// TODO: Consider listing supported formats
+		return fmt.Errorf(`format "%s" is not supported`, format)
 	}
 
 	outputPath := path.Join(path.Dir(inputPath), "CONVERTED_"+path.Base(inputPath))
@@ -71,7 +69,7 @@ func (c *convert) Run(cli *CLI) int {
 	w := transaction.NewCSVWriter(out)
 	err = w.WriteAll(transactions)
 	if err != nil {
-		cli.err.Printf(`could not write converted file to %s: %v`, outputPath, err)
+		return fmt.Errorf(`could not write converted file to %s: %v`, outputPath, err)
 	}
-	return 0
+	return nil
 }
